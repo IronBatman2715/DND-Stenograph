32bf7e791d902ba67734a7d56211b62093a947bb
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2021-10-02 11:45:12 -0400

    1.6.4
    -Changed name of "jsonTransfer.js" to more appropriate name of "jsonHandler.js"
    -Character name functions consolidated into one function
    -Moved Load data function in-line of html element
    -Streamlined updateStats and updateSaves to only check specified stat that was changed
    -Made calculated input elements read only
    -Proficiency progression is now customizable (read from options.json)
    -Stat modifier progression/function is now customizable (read from options.json)
    -Print options.json values to console and verify validity upon load
    -Formatting/Syntax

--- autoMath.js
+++ autoMath.js
@@ -10,7 +10,7 @@ function clearData() {
   }
 }
 
-function updateStats() {
+function updateStats(statNum) {
   let stats = document.getElementsByClassName("stat");
   let statmods = document.getElementsByClassName("statmod");
   let statNames = [
@@ -21,65 +21,74 @@ function updateStats() {
     "Intelligence",
     "Charisma",
   ];
-  let isAboveTypStatMax = false;
+  let isAboveTypMax = false;
+  let typMaxEqualsMax = options.stat.typMax == null;
 
-  //Iterate through all 6 stats/statmods
-  for (let i = 0; i < 6; i++) {
-    let stat = parseInt(stats[i].value);
-    let isStatValid = statLimits.min <= stat && stat <= statLimits.absmax;
+  let stat = parseInt(stats[statNum].value);
+  let isStatValid = options.stat.min <= stat && stat <= options.stat.max;
 
-    //Check for valid input
-    if (Number.isInteger(stat) && isStatValid) {
-      //Check if above usual values
-      if (stat > statLimits.typmax) {
-        isAboveTypStatMax = true;
-      }
+  //Check for valid input
+  if (Number.isInteger(stat) && isStatValid) {
+    //Check if above usual values
+    if (stat > options.stat.typMax) {
+      isAboveTypMax = true;
+    }
 
-      //Calculate modifier
-      let calcMod = Math.floor((stat - 10) / 2);
-      if (0 <= calcMod) {
-        //Positive modifier
-        statmods[i].value = "+" + calcMod.toString();
-      } else {
-        //Negative modifier
-        statmods[i].value = calcMod.toString();
-      }
+    //Calculate modifier
+    let calcMod = statModFunction(stat);
+
+    //Add prefix to modifier
+    if (0 <= calcMod) {
+      //Positive modifier
+      statmods[statNum].value = "+" + calcMod.toString();
     } else {
-      //Invalid input
-      statmods[i].value = "";
+      //Negative modifier
+      statmods[statNum].value = calcMod.toString();
+    }
+  } else {
+    //Invalid input
+    statmods[statNum].value = "";
 
-      //If stat value is not empty, send invalid input alert
-      if (!!stats[i].value) {
+    //If stat value is not empty, send invalid input alert
+    if (!!stats[statNum].value) {
+      if (typMaxEqualsMax) {
         alert(
-          `Invalid ${statNames[i]} value: ${stats[i].value}\n\nAll stat values must be integers from ${statLimits.min} to ${statLimits.absmax}. Note that you can only go above ${statLimits.typmax} when explicitly told you can do so!`
+          `Invalid ${statNames[statNum]} value: ${stats[statNum].value}\n\nAll stat values must be integers from ${options.stat.min} to ${options.stat.max}.`
+        );
+      } else {
+        alert(
+          `Invalid ${statNames[statNum]} value: ${stats[statNum].value}\n\nAll stat values must be integers from ${options.stat.min} to ${options.stat.max}. Note that you can only go above ${options.stat.typMax} when explicitly told you can do so!`
         );
       }
     }
   }
   //Warn user about going above usual values
-  if (isAboveTypStatMax) {
+  if (isAboveTypMax && !typMaxEqualsMax) {
     alert(
-      `Note that any stat can NOT go above ${statLimits.typmax} unless EXPLICITLY told you can do so. Be sure you have something that allows that!`
+      `Note that any stat can NOT go above ${options.stat.typMax} unless EXPLICITLY told you can do so. Be sure you have something that allows that!`
     );
   }
 
-  updateSaves();
+  updateSaves(statNum);
   updateSkills();
 }
 
-function updateSaves() {
+function updateSaves(statNum) {
   let statmods = document.getElementsByClassName("statmod");
   let profbonus = document.getElementsByClassName("profbonus");
   let saves = document.getElementsByClassName("save");
   let saveprofs = document.getElementsByClassName("saveprof");
 
-  //Iterate through all 6 stats/saves
-  for (let i = 0; i < 6; i++) {
-    let statmod = parseInt(statmods[i].value);
-    let saveprof = saveprofs[i].checked;
+  if (statNum == "all") {
+    for (let i = 0; i < 6; i++) {
+      updateSaves(i);
+    }
+  } else {
+    let statmod = parseInt(statmods[statNum].value);
+    let saveprof = saveprofs[statNum].checked;
 
     if ((!statmod && 0 !== statmod) || !profbonus[0].value) {
-      saves[i].value = "";
+      saves[statNum].value = "";
     } else {
       let save;
       if (saveprof) {
@@ -89,11 +98,11 @@ function updateSaves() {
       }
 
       if (!save && save !== 0) {
-        saves[i].value = "";
+        saves[statNum].value = "";
       } else if (0 <= save) {
-        saves[i].value = "+" + save.toString();
+        saves[statNum].value = "+" + save.toString();
       } else {
-        saves[i].value = save.toString();
+        saves[statNum].value = save.toString();
       }
     }
   }
@@ -222,25 +231,30 @@ function updateSkills() {
 function level2ProfBonus() {
   let classlevel = document.getElementsByClassName("classlevel");
   let classlevelStr = classlevel[0].value;
-  console.log(classlevelStr);
+  console.log(`Reading class level(s) from string: ${classlevelStr}`);
 
-  //verify string is NOT empty
+  //verify string is NOT empty. if is empty, do nothing
   if (!!classlevelStr) {
-    let invalidSyntax = false;
+    let invalidSyntax = false; //assume valid syntax until proven otherwise
+    let invalidIndex = null; //index that threw invalidSyntax flag
     let profbonus = document.getElementsByClassName("profbonus");
-    let totalLevel = 0;
-    let isReadingDigits = false;
-    let n = 1;
-    let nthClassLevel = "";
+    let totalLevel = 0; //total character level
+    let isReadingDigits = false; //currently parsing through level number(s)
+    let n = 1; //nth class parsed through
+    let classes = Array(1).fill({ name: "", level: "", startIndex: 0 }); //array of objects of class information
 
     //iterate through each character of classlevelStr, checking for key characters
     for (let i = 0; i < classlevelStr.length; i++) {
+      //i is current character of classlevelStr
+
       if (isReadingDigits) {
         //if currrently reading digits
+
         if (classlevelStr[i] == " ") {
           //check for invalid syntax
-          console.log("Space before slash!");
+          console.log("\tSpace before slash!");
           invalidSyntax = true;
+          invalidIndex = i;
           break;
         } else if (classlevelStr[i] == "/" || i == classlevelStr.length - 1) {
           //check for end cases
@@ -248,35 +262,50 @@ function level2ProfBonus() {
           //if last character, verify and read
           if (i == classlevelStr.length - 1) {
             if ("0" <= classlevelStr[i] && classlevelStr[i] <= "9") {
-              nthClassLevel = nthClassLevel.concat(classlevelStr[i]);
+              classes[n - 1].level = classes[n - 1].level.concat(
+                classlevelStr[i]
+              );
             } else {
-              console.log("Nonnumeric character after space at end!");
+              console.log("\tNonnumeric character after space at end!");
               invalidSyntax = true;
+              invalidIndex = i;
               break;
             }
           }
 
           //verify class level and add to total if valid
-          nthClassLevel = parseInt(nthClassLevel);
-          console.log(`Class ${n}'s level: ${nthClassLevel}`);
-          if (isValidLevel(nthClassLevel, "class level", n)) {
-            totalLevel = totalLevel + nthClassLevel;
+          classes[n - 1].level = parseInt(classes[n - 1].level);
+          if (isValidLevel(classes[n - 1].level, "class level", n)) {
+            totalLevel = totalLevel + classes[n - 1].level;
           } else {
             profbonus[0].value = "";
             return;
           }
 
+          console.log(
+            `\tClass ${n}\n\t\tname: ${classes[n - 1].name}\n\t\tlevel: ${
+              classes[n - 1].level
+            }\n\t\tstartIndex: ${classes[n - 1].startIndex}`
+          );
+
           //reset for next class
           n = n + 1;
-          nthClassLevel = "";
           isReadingDigits = false;
+
+          //if NOT last character, add next classes index
+          if (i != classlevelStr.length - 1) {
+            classes.push({ name: "", level: "", startIndex: i + 1 });
+          }
         } else {
           //no end cases or invalid syntax, verify and read digit character
           if ("0" <= classlevelStr[i] && classlevelStr[i] <= "9") {
-            nthClassLevel = nthClassLevel.concat(classlevelStr[i]);
+            classes[n - 1].level = classes[n - 1].level.concat(
+              classlevelStr[i]
+            );
           } else {
             console.log("Nonnumeric character after space and before slash!");
             invalidSyntax = true;
+            invalidIndex = i;
             break;
           }
         }
@@ -286,52 +315,54 @@ function level2ProfBonus() {
           //check for invalid syntax
           console.log("Slash NOT after number!");
           invalidSyntax = true;
+          invalidIndex = i;
           break;
         } else if (classlevelStr[i] == " ") {
           //check for flag to start reading digits
-          console.log(`Start reading digits at index ${i}`);
           isReadingDigits = true;
         } else {
-          //no end cases or invalid syntax, go to next character
+          //no end cases or invalid syntax, enter character into nth class name
+          classes[n - 1].name = classes[n - 1].name.concat(classlevelStr[i]);
         }
       }
     }
 
+    //console.log(classes);
+
     if (invalidSyntax) {
       alert(
-        "Invalid Class & Level syntax! Explicitly format as follows with NO spaces or slashes unless specified. Replace parentheses and their contents with values:\n\n(class name 1)(space)(class 1's level)(slash)(repeat format, ending last WITHOUT a slash)"
+        `Invalid Class & Level syntax (flagged at character ${
+          invalidIndex + 1
+        })! Explicitly format as follows with NO spaces or slashes unless specified. Replace parentheses and their contents with values:\n\n(class name 1)(space)(class 1's level)(slash)(repeat format, ending last WITHOUT a slash)`
       );
       profbonus[0].value = "";
       return;
     }
 
     if (isValidLevel(totalLevel, "total character level", 0)) {
-      console.log(`Total character level: ${totalLevel}`);
-      /* Hard coded proficiency bonus progression according to official rules */
-      if (1 <= totalLevel && totalLevel <= 4) {
-        profbonus[0].value = "+2";
-      } else if (5 <= totalLevel && totalLevel <= 8) {
-        profbonus[0].value = "+3";
-      } else if (9 <= totalLevel && totalLevel <= 12) {
-        profbonus[0].value = "+4";
-      } else if (13 <= totalLevel && totalLevel <= 16) {
-        profbonus[0].value = "+5";
-      } else if (totalLevel <= 20) {
-        profbonus[0].value = "+6";
-      } else {
-        profbonus[0].value = "invalid";
+      console.log(`\tTotal character level: ${totalLevel}`);
+      // Proficiency bonus progression
+      for (let i = 0; i < options.proficiencyProgression.length; i++) {
+        if (
+          options.proficiencyProgression[i].levelRange[0] <= totalLevel &&
+          totalLevel <= options.proficiencyProgression[i].levelRange[1]
+        ) {
+          profbonus[0].value = options.proficiencyProgression[i].bonus;
+
+          updateSaves("all");
+          updateSkills();
+          return;
+        }
       }
+      profbonus[0].value = "invalid";
     } else {
-      profbonus[0].value = "";
+      profbonus[0].value = "invalid";
       return;
     }
-
-    updateSaves();
-    updateSkills();
   }
 
   //Check validity of input level helper function
-  function isValidLevel(level, str, classNum) {
+  function isValidLevel(level, levelDescriptorStr, classNum) {
     //Ensure correct ordinal suffix
     if (classNum > 0) {
       let num;
@@ -349,39 +380,42 @@ function level2ProfBonus() {
           num = `${classNum}th `;
           break;
       }
-      str = num.concat(str);
+      levelDescriptorStr = num.concat(levelDescriptorStr);
     }
 
-    if (level > levelLimits.max) {
-      //level is above maximum
+    //Validity tests
+    if (level > options.characterLevel.max) {
+      //level is above maximum, alert user and mark as invalid
       alert(
-        `Invalid ${str}!\n\nMust be no more than ${levelLimits.max} (Current: ${level})!`
+        `Invalid ${levelDescriptorStr}!\n\nMust be no more than ${options.characterLevel.max} (Current: ${level})!`
       );
       return false;
-    } else if (level < levelLimits.min) {
-      //level is below minimum
+    } else if (level < options.characterLevel.min) {
+      //level is below minimum, alert user and mark as invalid
       alert(
-        `Invalid ${str}!\n\nMust be at least ${levelLimits.min} (Current: ${level})!`
+        `Invalid ${levelDescriptorStr}!\n\nMust be at least ${options.characterLevel.min} (Current: ${level})!`
       );
       return false;
     } else {
-      return true;
+      return true; //valid level, proceed
     }
   }
 }
 
-function charNameTop() {
+function charName(num) {
   let charnametop = document.getElementsByClassName("charnametop");
   let charnamebot = document.getElementsByClassName("charnamebot");
 
-  charnamebot[0].value = charnametop[0].value;
-}
-
-function charNameBot() {
-  let charnametop = document.getElementsByClassName("charnametop");
-  let charnamebot = document.getElementsByClassName("charnamebot");
-
-  charnametop[0].value = charnamebot[0].value;
+  switch (num) {
+    case 0:
+      charnamebot[0].value = charnametop[0].value;
+      break;
+    case 1:
+      charnametop[0].value = charnamebot[0].value;
+      break;
+    default:
+      break;
+  }
 }
 
 function deathsaves() {
---
+++ jsonHandler.js
@@ -0,0 +1,256 @@
+//Import options.json and parse values to global variables used in autoMath.js
+let options;
+let defaultOptionsLocation = "./options.json";
+/** Global options variables used throughout JS code (mostly autoMath.js)
+ * options.stenographVersion;
+ * options.version;
+ * options.stat;
+ * options.characterLevel;
+ * options.proficiencyProgression
+ **/
+let statModFunction; //function to convert from stat to stat modifier
+
+function getOptionsJSON(callback, optionsLocation) {
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", optionsLocation, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+//Load data from json options file
+function loadOptions(optionsLocation) {
+  getOptionsJSON(function (response) {
+    options = JSON.parse(response); //load data from json
+
+    /** Versions **/
+    console.log(`Stenograph Version: ${stenographVersion}\n`); //currently running version of Stenograph
+    console.log(`Loading Options file: ${optionsLocation}`); //currently loaded options file
+    console.log(`\tOptions Stenograph Version: ${options.stenographVersion}`); //version of Stenograph this options file was designed on
+    console.log(`\tOptions Version: ${options.version}`); //version of options file
+
+    /** Data **/
+    //characterLevel
+    console.log(
+      `\tCharacter Level:\n\t\tmin: ${options.characterLevel.min}\n\t\tmax: ${options.characterLevel.max}`
+    );
+
+    //proficicncyProgression
+    console.log("\tProficiency progression:");
+    for (let i = 0; i < options.proficiencyProgression.length; i++) {
+      console.log(
+        `\t\tbonus: ${options.proficiencyProgression[i].bonus}\n\t\tlevelRange: ${options.proficiencyProgression[i].levelRange[0]} to ${options.proficiencyProgression[i].levelRange[1]}\n`
+      );
+    }
+
+    /* stat */
+    //stat.min, stat.typMax, stat.max
+    console.log(
+      `\tStat Limits:\n\t\tmin: ${options.stat.min}\n\t\ttypMax: ${options.stat.typMax}\n\t\tmax: ${options.stat.max}`
+    );
+    //modFunction: Assume all values MUST be integers
+    statModFunction = new Function(
+      options.stat.modFunction.argument,
+      options.stat.modFunction.body
+    );
+    console.log(
+      `\tStat Modifer Function:\n\t\t${options.stat.modFunction.body}`
+    );
+
+    /** Verify **/
+    verifyOptions(optionsLocation);
+  }, optionsLocation);
+}
+
+//Verify that options file has valid values
+function verifyOptions(optionsLocation) {
+  let invalid = false;
+
+  //Version
+  if (options.stenographVersion !== stenographVersion) {
+    alert(
+      `Different version warning!\n\nThe options data used was made on an different version of Stenograph (Current: ${stenographVersion}; Your version: ${options.stenographVersion}). Some automatic math calculations might work incorrectly!`
+    );
+  }
+
+  //characterLevel: ensure consecutive order
+  if (options.characterLevel.min >= options.characterLevel.max) {
+    invalidOptions("characterLevel");
+  }
+
+  //proficiencyProgression
+  for (let i = 0; i < options.proficiencyProgression.length; i++) {
+    //ensure consecutive order in levelRange
+    if (
+      options.proficiencyProgression[i].levelRange[0] >
+      options.proficiencyProgression[i].levelRange[1]
+    ) {
+      invalidOptions("proficiencyProgression.levelRange");
+    } else if (i != 0) {
+      //ensure consecutive order in bonus
+      if (
+        parseInt(options.proficiencyProgression[i - 1].bonus[1]) >=
+        parseInt(options.proficiencyProgression[i].bonus[1])
+      ) {
+        invalidOptions("proficiencyProgression.bonus");
+      }
+
+      //ensure consecutive order in levelRange between entries
+      if (
+        options.proficiencyProgression[i - 1].levelRange[1] + 1 !=
+        options.proficiencyProgression[i].levelRange[0]
+      ) {
+        invalidOptions("proficiencyProgression.levelRange");
+      }
+    }
+  }
+
+  /* stat */
+  //stat.min, stat.typMax, stat.max: ensure consecutive order & typMax is null if not below max
+  if (options.stat.typMax == null) {
+    if (options.stat.min >= options.stat.max) {
+      invalidOptions("stat");
+    }
+  } else {
+    if (
+      options.stat.min >= options.stat.typMax ||
+      options.stat.typMax >= options.stat.max
+    ) {
+      invalidOptions("stat");
+    }
+  }
+
+  //stat.modFunction: assume all returned values must be integers
+  for (let stat = options.stat.min; stat <= options.stat.max; stat++) {
+    if (!Number.isInteger(statModFunction(stat))) {
+      invalidOptions(
+        "stat.modFunction(must yield integers for all values in stat range)"
+      );
+      break;
+    }
+  }
+
+  if (!invalid) {
+    console.log(`Options file(${optionsLocation}) is valid!`);
+  } else {
+    console.log(
+      `Options file(${optionsLocation}) is invalid! Refer to alert(s) that popped up.`
+    );
+  }
+
+  /* Invalid data alert helper function */
+  function invalidOptions(str) {
+    alert(
+      `Invalid options file! Check your values!\n\nInvalid ${str} value(s)`
+    );
+    invalid = true;
+  }
+}
+
+//Saving character data
+function saveData() {
+  let elementValueObj = new Object();
+  //Save Stenogrpah version number
+  elementValueObj["stenographVersion"] = stenographVersion;
+
+  //Save skill proficiencies
+  let skillprof = document.querySelectorAll("div.skillprofbox div");
+  for (let i = 0; i < skillprof.length; i++) {
+    //console.log(skillprof[i].getAttribute("class"));
+    elementValueObj[skillprof[i].getAttribute("name")] =
+      skillprof[i].getAttribute("class");
+  }
+
+  //Save rest of inputs
+  let charsheet = document.getElementById("charsheet");
+  for (let i = 0; i < charsheet.length; i++) {
+    let desValue;
+    if (charsheet[i].type == "text" || charsheet[i].type == "textarea") {
+      desValue = charsheet[i].value;
+    } else if (charsheet[i].type == "checkbox") {
+      desValue = charsheet[i].checked;
+    }
+
+    elementValueObj[charsheet[i].name] = desValue;
+  }
+
+  jsonText = JSON.stringify(elementValueObj); //translate object to JSON
+
+  //Prompt user with download
+  let a = document.createElement("a");
+  a.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(jsonText)
+  );
+  if (!charsheet[0].value) {
+    a.setAttribute("download", "stenograph_character.json");
+  } else {
+    a.setAttribute("download", `stenograph_${charsheet[0].value}.json`);
+  }
+  a.click();
+}
+
+//Once file has been uploaded, run loadData
+$(document).ready(function () {
+  $(".loaddata").change(function () {
+    loadData();
+  });
+});
+
+//Loading character data
+function loadData() {
+  let loadDataButton = document.getElementsByClassName("loaddata");
+  let files = loadDataButton[0].files;
+
+  if (files.length !== 1) {
+    //Invalid upload
+    alert("Submit exactly one .json or .txt!");
+    return false;
+  } else {
+    let fr = new FileReader();
+
+    fr.onload = function (e) {
+      let result = JSON.parse(e.target.result);
+      let jsonText = JSON.stringify(result, null, 2);
+      let dataObj = JSON.parse(jsonText);
+
+      //Check stenograph version of loaded data
+      if (dataObj["stenographVersion"] !== stenographVersion) {
+        alert(
+          `Different version warning!\n\nThis character sheet was made on an different version of Stenograph (Current Stenograph version: ${stenographVersion}; Your character sheet's Stenograph version: ${dataObj["stenographVersion"]}). Some data may not transfer or may transfer incorrectly!\n\nTypically, this will happen between feature updates: when the second number in the verison number changes. Ex: 1.4.0 -> 1.5.0.`
+        );
+      }
+
+      //Load skill proficiencies
+      let skillprof = document.querySelectorAll("div.skillprofbox div");
+      for (let i = 0; i < skillprof.length; i++) {
+        skillprof[i].className = dataObj[skillprof[i].getAttribute("name")];
+      }
+
+      //Load rest of inputs
+      let charsheet = document.getElementById("charsheet");
+      for (let i = 0; i < charsheet.length; i++) {
+        let value = dataObj[charsheet[i].name];
+        if (charsheet[i].type == "text" || charsheet[i].type == "textarea") {
+          if (!value && value !== 0) {
+            //value DNE
+            value = "";
+          }
+          charsheet[i].value = value;
+        } else if (charsheet[i].type == "checkbox") {
+          if (!value && value !== 0) {
+            //value DNE
+            value = false;
+          }
+          charsheet[i].checked = value;
+        }
+      }
+    };
+
+    fr.readAsText(files.item(0));
+  }
+}
